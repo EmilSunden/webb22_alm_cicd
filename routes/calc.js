5f8c75e5334184a897c13a177a144102e62f0bb6
EmilSunden <emilsunjobb@gmail.com>
2023-05-15 12:12:07 +0200

    Imported Calculator class, initialized a new instance of the Calc class

--- routes/calc.js
+++ routes/calc.js
@@ -2,9 +2,24 @@ const express = require('express');
 
 const router = express.Router();
 
+const Calculator = require('../src/calculator');
+
+const myCalc = new Calculator();
+
+
 /* GET home page. */
-router.get('/', (req, res) => {
-  res.send('Calculator route');
+router.get('/', (req, res, next) => {
+  const x = Number(req.query.value1);
+  const y = Number(req.query.value2);
+
+  res.render('calculator', {
+    val1: x,
+    val2: y,
+    add: myCalc.add(x, y),
+    subtract: myCalc.subtract(x, y),
+    multiply: myCalc.multiply(x, y),
+    divide: myCalc.divide(x, y),
+  });
 });
 
 module.exports = router;
